21723ef7f74ab4f6fb9f2e65eff76865698c8eae
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2021-02-22 22:38:19 -0800

    project initialization

---
+++ app/src/main/java/com/hanseltritama/amccloneapp/utils/Credentials.java
@@ -0,0 +1,7 @@
+package com.hanseltritama.amccloneapp.utils;
+
+public class Credentials {
+
+    public static final String BASE_URL = "https://api.themoviedb.org/";
+    public static final String API_KEY = "1b6bccfa81825771cd296691cf2de775";
+}
