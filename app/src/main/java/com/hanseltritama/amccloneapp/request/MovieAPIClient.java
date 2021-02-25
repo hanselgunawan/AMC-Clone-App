6b9ea0f72c2c7cd6527d3e4c5f71f2d6e01ecd3d
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2021-02-24 22:14:13 -0800

    added appexecutors / future

--- app/src/main/java/com/hanseltritama/amccloneapp/request/MovieAPIClient.java
+++ app/src/main/java/com/hanseltritama/amccloneapp/request/MovieAPIClient.java
@@ -1,8 +1,11 @@
 package com.hanseltritama.amccloneapp.request;
 
+import com.hanseltritama.amccloneapp.AppExecutors;
 import com.hanseltritama.amccloneapp.model.MovieModel;
 
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
@@ -27,6 +30,24 @@ public class MovieAPIClient {
 
     public LiveData<List<MovieModel>> getMovies() {
         return moviesLiveData;
+    }
+
+    public void searchMovieAPI() {
+        final Future myHandler = AppExecutors.getInstance().networkIO().submit(new Runnable() {
+            @Override
+            public void run() {
+                // Retrieve Data from API
+
+            }
+        });
+
+        AppExecutors.getInstance().networkIO().schedule(new Runnable() {
+            @Override
+            public void run() {
+                // Cancelling Retrofit Call
+                myHandler.cancel(true);
+            }
+        }, 4000, TimeUnit.MICROSECONDS);
     }
 
 }
