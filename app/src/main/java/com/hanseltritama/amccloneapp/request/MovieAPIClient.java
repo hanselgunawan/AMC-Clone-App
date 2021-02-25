5443340eaf426dd94f0df34cc0e7ef065f1f9c72
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2021-02-24 23:55:37 -0800

    added inner class

--- app/src/main/java/com/hanseltritama/amccloneapp/request/MovieAPIClient.java
+++ app/src/main/java/com/hanseltritama/amccloneapp/request/MovieAPIClient.java
@@ -1,21 +1,31 @@
 package com.hanseltritama.amccloneapp.request;
 
+import android.util.Log;
+
 import com.hanseltritama.amccloneapp.AppExecutors;
 import com.hanseltritama.amccloneapp.model.MovieModel;
+import com.hanseltritama.amccloneapp.response.MovieSearchResponse;
+import com.hanseltritama.amccloneapp.utils.Credentials;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import retrofit2.Call;
+import retrofit2.Response;
 
 public class MovieAPIClient {
 
     // LiveData
     private MutableLiveData<List<MovieModel>> moviesLiveData;
-
     private static MovieAPIClient instance;
+
+    // Global RUNNABLE Request
+    private RetrieveMoviesRunnable retrieveMoviesRunnable;
 
     public static MovieAPIClient getInstance() {
         if (instance == null) {
@@ -32,14 +42,14 @@ public class MovieAPIClient {
         return moviesLiveData;
     }
 
-    public void searchMovieAPI() {
-        final Future myHandler = AppExecutors.getInstance().networkIO().submit(new Runnable() {
-            @Override
-            public void run() {
-                // Retrieve Data from API
+    public void searchMovieAPI(String query, int pageNumber) {
+        if (retrieveMoviesRunnable != null) {
+            retrieveMoviesRunnable = null;
+        }
 
-            }
-        });
+        retrieveMoviesRunnable = new RetrieveMoviesRunnable(query, pageNumber);
+
+        final Future myHandler = AppExecutors.getInstance().networkIO().submit(retrieveMoviesRunnable);
 
         AppExecutors.getInstance().networkIO().schedule(new Runnable() {
             @Override
@@ -47,7 +57,64 @@ public class MovieAPIClient {
                 // Cancelling Retrofit Call
                 myHandler.cancel(true);
             }
-        }, 4000, TimeUnit.MICROSECONDS);
+        }, 5000, TimeUnit.MICROSECONDS);
+    }
+
+    private class RetrieveMoviesRunnable implements Runnable {
+
+        private String query;
+        private int pageNumber;
+        private boolean cancelRequest;
+
+        public RetrieveMoviesRunnable(String query, int pageNumber) {
+            this.query = query;
+            this.pageNumber = pageNumber;
+            cancelRequest = false;
+        }
+
+        @Override
+        public void run() {
+            // Getting the response objects
+            try {
+                Response response = getMovies(query, pageNumber).execute();
+                if (cancelRequest) {
+                    return;
+                }
+                if (response.code() == 200) {
+                    List<MovieModel> list = new ArrayList<>(((MovieSearchResponse) response.body()).getMovieList());
+                    if (pageNumber == 1) {
+                        // Sending data to live data
+                        // PostValue: used for background thread
+                        // setValue: not for background thread
+                        moviesLiveData.postValue(list);
+                    } else {
+                        List<MovieModel> currentMovies = moviesLiveData.getValue();
+                        currentMovies.addAll(list);
+                        moviesLiveData.postValue(currentMovies);
+                    }
+                } else {
+                    String error = response.errorBody().string();
+                    Log.v("Tag", "Error " + error);
+                    moviesLiveData.postValue(null);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                moviesLiveData.postValue(null);
+            }
+        }
+
+        private Call<MovieSearchResponse> getMovies(String query, int pageNumber) {
+            return Service.getMovieApi().searchMovie(
+                    Credentials.API_KEY,
+                    query,
+                    pageNumber
+            );
+        }
+
+        private void cancelRequest() {
+            Log.v("Tag", "Cancelling Search Request");
+            cancelRequest = true;
+        }
     }
 
 }
